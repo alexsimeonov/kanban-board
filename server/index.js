0855adf919b9b0e334fa796bd0c1067fec88d849
Alexander Simeonov <alex.simeonov@hotmail.com>
2021-10-04 22:18:51 +0300

    add restify-cors-middleware to the project

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-const restify = require("restify");
-const columnsService = require("./columns/columns");
-const cardsService = require("./cards/cards");
+const restify = require('restify');
+const corsMiddleware = require('restify-cors-middleware');
+const columnsService = require('./columns/columns');
+const cardsService = require('./cards/cards');
 
 const server = restify.createServer();
 const port = 3000;
@@ -9,9 +10,17 @@ const url = `http://localhost:${port}`;
 server.use(
   restify.plugins.bodyParser({
     mapParams: true,
-  })
+  }),
 );
+
 server.use(restify.plugins.queryParser());
+
+const cors = corsMiddleware({
+  origins: ['http://localhost:3001'],
+});
+
+server.pre(cors.preflight);
+server.use(cors.actual);
 
 server.listen(port, () => {
   console.log(`Server running on ${url}.`);
