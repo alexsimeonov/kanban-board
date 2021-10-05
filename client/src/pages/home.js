0286fd721243ec35a889e0d73d886867a5d53710
Alexander Simeonov <alex.simeonov@hotmail.com>
2021-10-05 15:24:02 +0300

    implement basic create column UI

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -8,6 +8,7 @@ const Home = styled.div`
   background-color: #F7F3E3;
   display: flex;
   justify-content: center;
+  flex-direction: column;
 `;
 
 const ColumnsContainer = styled.div`
@@ -19,21 +20,58 @@ const ColumnsContainer = styled.div`
   margin: 20px;
 `;
 
+const CreateColumnContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 10px;
+`;
+
+const ColumnNameInput = styled.input`
+  color: black;
+  width: 200px;
+  margin-right: 15px;
+  background-color: transparent;
+`;
+
+const CreateColumnButton = styled.button`
+  border: 1px solid black;
+  background-color: transparent;
+  width: 100px;
+  height: 20px;
+`;
+
 const HomePage = React.memo(() => {
   const [columns, setColumns] = useState([]);
+  let columnNameInput = '';
 
   const getColumns = () => axios.get('http://localhost:3000/columns').then((res) => {
     setColumns(res.data);
+  }).catch((err) => console.log(err));
+
+  const addColumn = (name) => axios.post('http://localhost:3000/columns', {
+    name,
+  }).then(() => {
+    columnNameInput = '';
   }).catch((err) => console.log(err));
 
   useEffect(() => {
     getColumns();
   }, []);
 
+  const changeColumnNameInput = (event) => {
+    columnNameInput = event.target.value;
+  };
+
   return (
     <div>
       <Header />
       <Home>
+        <CreateColumnContainer>
+          <ColumnNameInput type="text" placeholder="Enter Column Name..." onChange={changeColumnNameInput} />
+          <CreateColumnButton onClick={() => addColumn(columnNameInput)}>
+            Add Column
+          </CreateColumnButton>
+        </CreateColumnContainer>
         <ColumnsContainer>
           {
             columns.map((column) => (
